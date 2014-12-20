1772aea5d5a00bdacb3baf83a5f29e286901f816
Cerek Hillen <crockeo@gmail.com>
2014-12-20 13:40:16 -0500

    Loosely defined the API for the parser section of things.

---
+++ src/parser.cpp
@@ -0,0 +1,21 @@
+#include "parser.hpp"
+
+//////////////
+// Includes //
+#include <istream>
+#include <string>
+
+#include "json.hpp"
+
+//////////
+// Code //
+
+// Parsing out a block of JSON from a string.
+JValue parseJSON(std::string str) {
+    return JValue();
+}
+
+// Parsing out a block of JSON from an istream.
+JValue parseJSON(std::istream stream) {
+    return JValue();
+}
---
+++ src/parser.hpp
@@ -0,0 +1,20 @@
+#ifndef _PARSER_HPP_
+#define _PARSER_HPP_
+
+//////////////
+// Includes //
+#include <istream>
+#include <string>
+
+#include "json.hpp"
+
+//////////
+// Code //
+
+// Parsing out a block of JSON from a string.
+JValue parseJSON(std::string);
+
+// Parsing out a block of JSON from an istream.
+JValue parseJSON(std::istream);
+
+#endif
