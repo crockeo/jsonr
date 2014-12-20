693ab4ab7ed8ec4dedee03407ba6e0284042569b
Cerek Hillen <crockeo@gmail.com>
2014-12-20 02:06:43 -0500

    Aside from it not compiling for reasons (maps of JValue, probably?) I have the structure of what I want.

--- src/json.hpp
+++ src/json.hpp
@@ -1,8 +1,16 @@
 #ifndef _JSON_HPP_
 #define _JSON_HPP_
 
+//////////////
+// Includes //
+#include <string>
+#include <vector>
+#include <map>
+
 //////////
 // Code //
+
+class JValue;
 
 // A type to represent a piece of JSON.
 enum JType {
@@ -10,7 +18,8 @@ enum JType {
     JARRAY,
     JNUMBER,
     JSTRING,
-    JBOOL
+    JBOOL,
+    JNULL
 };
 
 // Declaring each of the types.
@@ -19,5 +28,45 @@ typedef std::vector<JValue>              JArray;
 typedef double                           JNumber;
 typedef std::string                      JString;
 typedef bool                             JBool;
+
+// A generic JSON value that can represent any type of JSON.
+class JValue {
+private:
+    JType type;
+    JObject jObject_value;
+    JArray jArray_value;
+    JNumber jNumber_value;
+    JString jString_value;
+    JBool jBool_value;
+
+public:
+    // Constructing a JValue as a couple of different types.
+    JValue(JObject);
+    JValue(JArray);
+    JValue(JNumber);
+    JValue(JString);
+    JValue(JBool);
+
+    // Constructing a JValue as null.
+    JValue();
+
+    // Getting the type of the JValue.
+    JType getType() const;
+
+    // Checking specifically if this JValue represents something else.
+    bool isObject() const;
+    bool isArray() const;
+    bool isNumber() const;
+    bool isString() const;
+    bool isBool() const;
+    bool isNull() const;
+
+    // Getting the specific values of this JValue.
+    JObject jObject() const;
+    JArray jArray() const;
+    JNumber jNumber() const;
+    JString jString() const;
+    JBool jBool() const;
+};
 
 #endif
