1fea176aee3cb18363b5678312a4ec38cf151c9c
Cerek Hillen <crockeo@gmail.com>
2014-12-21 04:30:00 -0500

    More testing stuff.

--- src/testing.cpp
+++ src/testing.cpp
@@ -2,15 +2,27 @@
 // Includes //
 #include <iostream>
 
+#include "testing/00.hpp"
+
 //////////
 // Code //
 
 // The entry point to the testing suite.
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "Proper usage: " << argv[0] << " <test number>" << std::endl;
+        std::cout << "Proper usage: " << argv[0] << " <test number>." << std::endl;
         return 1;
     }
 
-    return 0;
+    int n = atoi(argv[1]);
+    switch (n) {
+    case 0:
+        return runTest00();
+        break;
+    default:
+        break;
+    }
+
+    std::cerr << "Invalid test number!" << std::endl;
+    return 1;
 }
