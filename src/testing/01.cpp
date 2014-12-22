93a1f4fec9e9f54d7188e8acf828052cc10626a3
Cerek Hillen <crockeo@gmail.com>
2014-12-21 21:22:45 -0500

    Implemented the test.

--- src/testing/01.cpp
+++ src/testing/01.cpp
@@ -1,9 +1,21 @@
 #include "01.hpp"
 
+//////////////
+// Includes //
+#include "../parser.hpp"
+#include "../json.hpp"
+
 //////////
 // Code //
 
 // Testing the parseJSON function for parsing a string.
 int runTest01() {
-    return 0;
+    JValue sVal = parseJSON("\"hello\"");
+    JValue sqVal = parseJSON("'word up'");
+
+    if (sVal.isString() && sVal.jString().compare("hello") == 0 &&
+        sqVal.isString() && sqVal.jString().compare("word up") == 0)
+        return 0;
+
+    return 1;
 }
