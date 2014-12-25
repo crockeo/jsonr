e841f448e463aa59a395a62bb93ca45804bda8c4
Cerek Hillen <crockeo@gmail.com>
2014-12-25 18:21:58 -0500

    Started doing some complex JSON parsing stuff.

--- src/testing/05.cpp
+++ src/testing/05.cpp
@@ -45,7 +45,37 @@ bool testSimpleObject(const JValue& simpleObject) {
 }
 
 // Testing the complex object.
+// Complex object structure:
+// {
+//   "_id": "549c7c943e7a710a50638c11",
+//   "index": 0,
+//   "guid": "a5847a1e-8897-4195-acd3-82d6270a68a1",
+//   "isActive": false,
+//   "balance": "$2,495.21",
+//   "picture": "http://placehold.it/32x32",
+//   "age": 25,
+//   "eyeColor": "blue",
+//   "name": "Kasey Bentely",
+//   "gender": "female",
+//   "company": "GINKOGENE",
+//   "email": "kaseybentley@ginkogene.com",
+//   "phone": "+1 (954) 503-2214",
+//   "address": "806 Hull Street, Chalfant, California, 8749",
+//   "about": "Sunt officia ex non tempor. Elit voluptate ea est aliquip laboris laborum in eu do elit reprehenderit ea. Irure proident dolore commodo tempor qui. Duis nostrud est reprehenderit ex est culpa commodo mollit excepteur sunt ex qui. Lorem id consectetur qui mollit. Anim commodo laborum eu do ea dolor laboris in proident Lorem sint anim.\r\n",
+//   "registered": "2014-10-21T10:42:13 +04:00",
+//   "latitude": 48.984494,
+//   "longitude": -81.8415,
+//   "tags": ["incididunt", "eu", "eiusmod", "nostrud", "tempor", "incididunt", "consectetur"],
+//   "friends": [{ "id": 0, "name": "Snow Williamson" }, { "id": 1, "name": "Christy Mcfadden" }, { "id": 2, "name": "Cathy Stone" }],
+//   "greeting": "Hello, Kasey Bentley! You have 6 unread messages.",
+//   "favoriteFruit": "strawberry"
+// }
 bool testComplexObject(const JValue& complexObject) {
+    if (!complexObject.isObject())
+        return true;
+
+    // TODO: Some actual testing here.
+
     return false;
 }
 
@@ -53,9 +83,7 @@ bool testComplexObject(const JValue& complexObject) {
 int runTest05() {
     JValue emptyObject = parseJSON("{ }");
     JValue simpleObject = parseJSON("{ \"test\": true }");
-
-    // TODO: Make the complex object a little less empty.
-    JValue complexObject = parseJSON("{}");
+    JValue complexObject = parseJSON("{ \"_id\": \"549c7c943e7a710a50638c11\", \"index\": 0, \"guid\": \"a5847a1e-8897-4195-acd3-82d6270a68a1\", \"isActive\": false, \"balance\": \"$2,495.21\", \"picture\": \"http://placehold.it/32x32\", \"age\": 25, \"eyeColor\": \"blue\", \"name\": \"Kasey Bentley\", \"gender\": \"female\", \"company\": \"GINKOGENE\", \"email\": \"kaseybentley@ginkogene.com\", \"phone\": \"+1 (954) 503-2214\", \"address\": \"806 Hull Street, Chalfant, California, 8749\", \"about\": \"Sunt officia ex non tempor. Elit voluptate ea est aliquip laboris laborum in eu do elit reprehenderit ea. Irure proident dolore commodo tempor qui. Duis nostrud est reprehenderit ex est culpa commodo mollit excepteur sunt ex qui. Lorem id consectetur qui mollit. Anim commodo laborum eu do ea dolor laboris in proident Lorem sint anim.\r\n\", \"registered\": \"2014-10-21T10:42:13 +04:00\", \"latitude\": 48.984494, \"longitude\": -81.8415, \"tags\": [ \"incididunt\", \"eu\", \"eiusmod\", \"nostrud\", \"tempor\", \"incididunt\", \"consectetur\" ], \"friends\": [ { \"id\": 0, \"name\": \"Snow Williamson\" }, { \"id\": 1, \"name\": \"Christy Mcfadden\" }, { \"id\": 2, \"name\": \"Cathy Stone\" } ], \"greeting\": \"Hello, Kasey Bentley! You have 6 unread messages.\", \"favoriteFruit\": \"strawberry\" }");
 
     if (testEmptyObject(emptyObject))
         return 1;
